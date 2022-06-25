ba11877dfe154bc5fe74a636229b468f5b99e9d0
nazurazu <nazifamahbub@gmail.com>
2022-06-25 01:27:03 -0400

    website update Jul 25

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,13 @@
 
 import { useState, useEffect } from 'react';
 import "./styles/Home.css";
+import "./images/mainphoto.jpg"
 
 function Home() {
 return (
     <div className="home_body">
         <div className="home_images">
-            
-        </div>
-        <div className="home_text">
+            <img src="https://github.com/nazurazu/portfolio-website/blob/main/src/components/images/IMG_5230.jpg?raw=true" alt="Logo" className="portrait"/>
             
         </div>
         <div className="home_artifacts">
